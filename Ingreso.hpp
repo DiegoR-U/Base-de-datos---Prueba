e69a4d152311b60d98e885c31e28db0bbdbae3bb
gabrielritz <diego.rivera@ucsp.edu.pe>
2024-11-04 09:46:32 -0500

    Ingreso.hpp

--- Ingreso.hpp
+++ Ingreso.hpp
@@ -2,11 +2,17 @@
 #include <fstream>
 class ingreso{
     public:
-        int bruto;
-        int gasto;
-        int neto;
-        int d;
-        int m;
-        int y;
-        ingreso();
+    float ingreso;
+    float gasto;
+    float neto;
+    int d;
+    int m;
+    int y;
+    ingreso();
+    editingreso();
+    editgasto();
+    editfecha():
+    editd();
+    editm();
+    edity();
 };
