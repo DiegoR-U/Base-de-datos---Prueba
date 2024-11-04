9f3f6f6fd4dece8e7a686dd7a80f8d2d06f783f1
NathaliFora <nathali.poccori@ucsp.edu.pe>
2024-11-03 21:58:47 -0500

    Add files via upload

--- Vehiculo.cpp
+++ Vehiculo.cpp
@@ -1,3 +1,83 @@
 #include "Vehiculo.hpp"
 #include <iostream>
-
+//Constructor
+Vehiculo::Vehiculo()
+{         
+	id = -1;
+	modelo = nullptr;
+	matricula = nullptr;
+	estado = nullptr;
+}
+//Método para agregar un nuevo auto
+void Vehiculo::agregarIDVehiculo()
+{
+	id++;
+}
+//Método para añadir el modelo del auto
+void Vehiculo::agregarModelo() 
+{
+	char longitudmax[50];
+	int cont{0};
+		
+	std::cout << "Ingrese el modelo del auto: ";
+	std::cin >> longitudmax;
+	std::cout << std::endl;
+	for(int i = 0; i<50; i++)
+	{
+		if(longitudmax[i] != '\0')
+		{
+			cont++;
+		}
+	}
+		
+	modelo = new char[cont + 1];
+	for(int i = 0; i < cont + 1; i++)
+	{
+		*(modelo + i) = longitudmax[i];
+	}
+}
+//Método para ingresar la placa del auto
+void Vehiculo::agregarMatricula()
+{
+	char placa[8];
+	std::cout << "Ingrese la placa o matricula del auto: ";
+	std::cin >> placa;
+	std::cout << std::endl;
+	
+	matricula = new char[8];
+	for(int i = 0; i < 8; i++)
+	{
+		*(matricula + i) = placa[i];
+	}
+}
+//Método para clasificarlo como disponible o no
+void Vehiculo::estadoVehiculo()	
+{
+	char longitudmax[15];
+	int cont{0};
+		
+	std::cout << "Ingrese la disponibilidad del auto: ";
+	std::cin >> longitudmax;
+	std::cout << std::endl;
+	for(int i = 0; i<15; i++)
+	{
+		if(longitudmax[i] != '\0')
+		{
+			cont++;
+		}
+	}
+		
+	estado = new char[cont + 1];
+	for(int i = 0; i < cont + 1; i++)
+	{
+		*(estado + i) = longitudmax[i];
+	}
+}
+//Método para imprimir en consola
+void Vehiculo::mostrarDatos()
+{
+	std::cout << "ID: " << id << std::endl;
+	std::cout << "Modelo: " << modelo << std::endl;
+	std::cout << "Matricula: " << matricula << std::endl;
+	std::cout << "Estado: " << estado << std::endl;
+}
--- Vehiculo.hpp
+++ Vehiculo.hpp
@@ -1,21 +1,17 @@
-#include <fstream>
-#include <iostream>
 class Vehiculo{
 public:
-	
-	explicit Vehiculo();
-	
-	void agregarVehiculo();
-	void quitarVehiculo();
-	void asignarVehiculo();
-	void modificarDatos();
-	void mostrarDatos();
-	
-private:
 
 	int id;
 	char *modelo;
 	char *matricula;
 	char *estado;
 	
+	Vehiculo();
+
+	void agregarIDVehiculo();
+	void agregarModelo();
+	void agregarMatricula();
+	void estadoVehiculo();
+	void mostrarDatos();
+
 };
