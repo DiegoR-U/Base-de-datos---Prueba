cb58f49a946acd7a8c3a3414e3d52cadee7a3b5b
gabrielritz <diego.rivera@ucsp.edu.pe>
2024-11-04 09:39:26 -0500

    Ingreso.cpp

--- Ingreso.cpp
+++ Ingreso.cpp
@@ -1,10 +1,27 @@
 #include "Ingreso.hpp"
 #include <iostream>
-ingreso::ingreso(int _bruto, int _gasto, int _d, int _m, int _y){
-    bruto = _bruto;
+ingreso::ingreso(float _ingreso, float _gasto, int _d, int _m, int _y){
+    ingreso = _ingreso;
     gasto = _gasto;
     neto = bruto - gasto;
     d = _d;
     m = _m;
     y = _y;
+}
+ingreso::editingreso(float x){
+    ingreso = x;
+}
+registro::editd(int x){
+  d = x;
+}
+registro::editm(int x){
+  m = x;
+}
+registro::edity(int x){
+  y = x;
+}
+registro::editfecha(int x, int y, int z){
+  editd(x);
+  editm(y);
+  edity(z);
 }
