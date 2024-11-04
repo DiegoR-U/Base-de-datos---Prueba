fb9a047252a20c8826d9a39c501d9abdc1ceb503
gabrielritz <diego.rivera@ucsp.edu.pe>
2024-11-04 09:14:40 -0500

    Ingreso.cpp

--- Ingreso.cpp
+++ Ingreso.cpp
@@ -1,5 +1,10 @@
 #include "Ingreso.hpp"
 #include <iostream>
-ingreso::ingreso(){
-  
+ingreso::ingreso(int _bruto, int _gasto, int _d, int _m, int _y){
+    bruto = _bruto;
+    gasto = _gasto;
+    neto = bruto - gasto;
+    d = _d;
+    m = _m;
+    y = _y;
 }
