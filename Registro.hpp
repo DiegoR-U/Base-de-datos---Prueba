4a3e0fe542f40fbe42cee20bc20a1651241e3141
gabrielritz <diego.rivera@ucsp.edu.pe>
2024-11-08 10:42:59 -0500

    Registro.hpp

--- Registro.hpp
+++ Registro.hpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <fstream>
 class registro{
-  public:
+private:
   char* nombre;
+  int nombrelen;
   int d;
   int m;
   int y;
   int inversion;
+public:
   registro();
-    edit()
+  borrarregistro();
+    edit();
       editnombre();
       editfecha():
         editd();
